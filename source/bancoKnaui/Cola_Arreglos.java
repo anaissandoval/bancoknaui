fbaf6df813aa089a61c00284b766adad8fa5b55b
ale.knaui <ale.knaui@593b13a8-4a04-b60c-aa23-457989d4260b>
2010-08-27 20:47:29 +0000

    Implementación de la cola con arreglos y pruebas unitarias implementadas para las colas.

---
+++ source/bancoKnaui/Cola_Arreglos.java
@@ -0,0 +1,66 @@
+package bancoKnaui;
+
+/**
+ * Cola implementada con arreglos
+ * @author aleKnaui
+ * @param <E> Genérico que indica el tipo de objetos contenido en la cola 
+ */
+public class Cola_Arreglos<E> extends AbstractCola<E> {
+	
+	// --------------------------------------------------
+	// Atributos
+	// --------------------------------------------------
+	
+	/** Los elementos contenidos en la cola */
+	private Object[] elementos;
+	
+	// --------------------------------------------------
+	// Constructor
+	// --------------------------------------------------
+	
+	/**
+	 * Crea la cola vacía
+	 */
+	public Cola_Arreglos(){
+		elementos = new Object[0];
+	}
+	
+	// --------------------------------------------------
+	// Métodos
+	// --------------------------------------------------
+
+	@Override
+	public E dequeue() {
+		if( isEmpty() ) return null;
+		else{
+			E retorno = (E) elementos[0];
+			Object[] nuevo = new Object[ size()-1 ];
+			for( int i = 1; i < size(); i++ )
+				nuevo[i-1] = elementos[i];
+			elementos =  nuevo;
+			return retorno;
+		}
+	}
+
+	@Override
+	public void enqueue(E elemento) {
+		Object[] nuevos = new Object[ size() + 1 ];
+		for( int i = 0; i < size(); i++ )
+			nuevos[i] = elementos[i];
+		nuevos[ nuevos.length - 1 ] = elemento;
+		elementos = nuevos;
+	}
+
+	@Override
+	public E peek() {
+		if( isEmpty() ) return null;
+		else
+			return (E) elementos[0];
+	}
+
+	@Override
+	public int size() {
+		return elementos.length;
+	}
+
+}
